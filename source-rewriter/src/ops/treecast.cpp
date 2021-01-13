40f98f6590e4370b8ec93e2b16e3ea1ff4cd7d1e
Guy Lüthi <guluethi@student.ethz.ch>
2021-01-13 10:46:13 +0100

    bugfix in source rewriter

--- source-rewriter/src/ops/treecast.cpp
+++ source-rewriter/src/ops/treecast.cpp
@@ -12,28 +12,28 @@ static OperationMetadata extractTreecast(CallExpr* channelDecl)
     );
 }
 
-OperationMetadata BarrierExtractor::GetOperationMetadata(CallExpr* callExpr)
+OperationMetadata TreecastExtractor::GetOperationMetadata(CallExpr* callExpr)
 {
-    return extractBarrier(extractChannelDecl(callExpr));
+    return extractTreecast(extractChannelDecl(callExpr));
 }
-std::string BarrierExtractor::CreateDeclaration(const std::string& callName, const OperationMetadata& metadata)
+std::string TreecastExtractor::CreateDeclaration(const std::string& callName, const OperationMetadata& metadata)
 {
     return "void " + this->RenameCall(callName, metadata) + "(SMI_TreecastChannel* chan, void* data);";
 }
-std::vector<std::string> BarrierExtractor::GetFunctionNames()
+std::vector<std::string> TreecastExtractor::GetFunctionNames()
 {
     return {"SMI_Treecast"};
 }
 
-OperationMetadata BarrierChannelExtractor::GetOperationMetadata(CallExpr* callExpr)
+OperationMetadata TreecastChannelExtractor::GetOperationMetadata(CallExpr* callExpr)
 {
-    return extractBarrier(callExpr);
+    return extractTreecast(callExpr);
 }
-std::string BarrierChannelExtractor::CreateDeclaration(const std::string& callName, const OperationMetadata& metadata)
+std::string TreecastChannelExtractor::CreateDeclaration(const std::string& callName, const OperationMetadata& metadata)
 {
     return this->CreateChannelDeclaration(callName, metadata, "SMI_TreecastChannel", "int count, SMI_Datatype data_type, int port, int root, SMI_Comm comm");
 }
-std::string BarrierChannelExtractor::GetChannelFunctionName()
+std::string TreecastChannelExtractor::GetChannelFunctionName()
 {
     return "SMI_Open_treecast_channel";
 }
